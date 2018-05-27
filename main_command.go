d5c067d528185dd0b58472766d63f015b51a8e01
ZeroMagic <anthonyliu@zju.edu.cn>
2018-05-27 21:17:00 +0800

    init custom
    Signed-off-by: ZeroMagic <anthonyliu@zju.edu.cn>

--- main_command.go
+++ main_command.go
@@ -8,35 +8,33 @@ import (
 )
 
 var runCommand = cli.Command{
-	Name: "run",
-	Usage: `Create a container with namespace and cgroups limit
-			mydocker run -ti [command]`,
+	Name:	"run",
+	Usage:	`mydocker run -ti [commond]`,
 	Flags: []cli.Flag{
 		cli.BoolFlag{
-			Name:  "ti",
-			Usage: "enable tty",
+			Name:	"ti",
+			Usage:	"enable tty",
 		},
 	},
 	Action: func(context *cli.Context) error {
 		if len(context.Args()) < 1 {
-			return fmt.Errorf("Missing container command")
+			return fmt.Errorf("Missing container commond")
 		}
-		var cmdArray []string
-		for _, arg := range context.Args() {
-			cmdArray = append(cmdArray, arg)
-		}
+		cmd := context.Args().Get(0)
 		tty := context.Bool("ti")
-		Run(tty, cmdArray)
+		Run(tty, cmd)
 		return nil
 	},
 }
 
 var initCommand = cli.Command{
-	Name:  "init",
-	Usage: "Init container process run user's process in container. Do not call it outside",
-	Action: func(context *cli.Context) error {
-		log.Infof("init come on")
-		err := container.RunContainerInitProcess()
+	Name:	"init",
+	Usage:	"init container",
+	Action:	func（context *cli.Context) error {
+		log.Info("init come on")
+		cmd := context.Args().Get(0)
+		log.Infof("command %s", cmd)
+		err := container.RunContainerInitProcess(cmd, nil)
 		return err
 	},
 }
--- run.go
+++ run.go
@@ -8,26 +8,11 @@ import (
 )
 
 
-func Run(tty bool, comArray []string) {
-	parent, writePipe := container.NewParentProcess(tty)
-	if parent == nil {
-		log.Errorf("New parent process error")
-		return
-	}
+func Run(tty bool, command string) {
+	parent := container.NewParentProcess(tty, command)
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
-	sendInitCommand(comArray, writePipe)
 	parent.Wait()
-	mntURL := "/root/mnt/"
-	rootURL := "/root/"
-	container.DeleteWorkSpace(rootURL, mntURL)
-	os.Exit(0)
-}
-
-func sendInitCommand(comArray []string, writePipe *os.File) {
-	command := strings.Join(comArray, " ")
-	log.Infof("command all is %s", command)
-	writePipe.WriteString(command)
-	writePipe.Close()
+	os.Exit(-1)
 }
